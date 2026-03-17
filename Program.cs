085c39d791757b1f3de7c11263da5c2f4bb1f62e
Wiktoria Piasecka <s33996@pjwstk.edu.pl>
2026-03-17 22:49:21 +0100

    Add input parsing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿Console.WriteLine("Statistics calculator");
 Console.WriteLine("Enter numbers separated by spaces:");
+
+string? input = Console.ReadLine();
+
+if (input != null)
+{
+    string[] values = input.Split(' ');
+    Console.WriteLine($"You entered {values.Length} values.");
+}
