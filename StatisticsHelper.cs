a4ead6ddb4e6730ce775ef80f6250a68ea343066
Wiktoria Piasecka <s33996@pjwstk.edu.pl>
2026-03-17 23:35:07 +0100

    Implement min calculation

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -34,6 +34,16 @@ public static class StatisticsHelper
 
     public static int CalculateMin(int[] values)
     {
-        return values[0];
+        int min = values[0];
+
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i] < min)
+            {
+                min = values[i];
+            }
+        }
+
+        return min;
     }
 }
